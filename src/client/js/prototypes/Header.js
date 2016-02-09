2faa3824f01a83deb3c7b666c0336318b1322f0f
albizures <albizures3601@gmail.com>
2016-02-09 17:54:22 -0600

    se agrego y posiciono en el header el nit y el telefono

--- src/client/js/prototypes/Header.js
+++ src/client/js/prototypes/Header.js
@@ -1,10 +1,13 @@
+'use strict';
+const moment = require("moment");
+
 function Header(dimensions, data) {
 	this.shape = new createjs.Shape();
 	this.fecha = new createjs.Text('Fecha: ' + data.fecha, "bold 13px Arial", "black");
 	this.nombre = new createjs.Text('Nombre: ' + data.nombre, "bold 13px Arial", "black");
 	this.direccion = new createjs.Text('Direccion: ' + data.direccion, "bold 13px Arial", "black");
-	// this.telefono = new createjs.Text('Telefono: ' + data.telefono, "bold 13px Arial", "black");
-	//this.nit = new createjs.Text('NIT: ' + data.nit, "bold 13px Arial", "black");
+	this.telefono = new createjs.Text('Telefono: ' + data.telefono, "bold 13px Arial", "black");
+	this.nit = new createjs.Text('NIT: ' + data.nit, "bold 13px Arial", "black");
 	this.dimensions = dimensions;
 	this.update();
 	this.updatePositionText();
@@ -30,11 +33,11 @@ Header.prototype.update = function () {
 	this.updatePositionText();
 };
 Header.prototype.updateText = function (data) {
-	this.fecha.text = 'Fecha: ' + data.fecha;
+	this.fecha.text = 'Fecha: ' + moment(data.fecha).format('LL');
 	this.nombre.text = 'Nombre: ' + data.nombre;
 	this.direccion.text = 'Direccion: ' +  data.direccion;
-	//this.telefono.text = data.telefono;
-	//this.nit.text = data.nit;
+	this.telefono.text = 'Telefono: ' + data.telefono;
+	this.nit.text = 'NIT: ' + data.nit;
 };
 Header.prototype.updatePositionText = function () {
 	let heightLine = this.dimensions.height / 3;
@@ -44,11 +47,14 @@ Header.prototype.updatePositionText = function () {
 	this.nombre.x = this.dimensions.x + 10;
 	this.nombre.y = heightLine * 2 - (heightLine / 1.6) + this.dimensions.y;
 
+	this.telefono.x = (this.dimensions.x + 10)  + this.dimensions.width * 0.5;
+	this.telefono.y = heightLine * 2 - (heightLine / 1.6) + this.dimensions.y;
+
 	this.direccion.x = this.dimensions.x + 10;
 	this.direccion.y = heightLine * 3 - (heightLine / 1.6) + this.dimensions.y;
 
-	// this.telefono.x = this.dimensions.x + 10;
-	// this.telefono.y = heightLine - 10;
+	this.nit.x = (this.dimensions.x + 10)  + this.dimensions.width * 0.5;
+	this.nit.y = heightLine * 3 - (heightLine / 1.6) + this.dimensions.y;
 };
 Header.prototype.addChildTo = function (stage) {
 	stage.addChild(this.shape);
